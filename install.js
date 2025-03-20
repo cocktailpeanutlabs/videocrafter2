789cc53c6c176d9a018127feaa2c3ac5243639f4
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-19 20:21:27 -0700

    update

--- install.js
+++ install.js
@@ -1,5 +1,5 @@
 module.exports = async (kernel) => {
-  let cmd = "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu"
+  let cmd = "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cpu"
   if (kernel.platform === 'darwin') {
     if (kernel.arch === "arm64") {
       cmd = "uv pip install torch torchaudio torchvision"
@@ -11,10 +11,10 @@ module.exports = async (kernel) => {
       if (kernel.gpu_model && / 50.+/.test(kernel.gpu_model)) {
         cmd = "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
       } else {
-        cmd = "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 xformers --index-url https://download.pytorch.org/whl/cu121"
+        cmd = "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 xformers --index-url https://download.pytorch.org/whl/cu124"
       }
     } else if (kernel.gpu === 'amd') {
-      cmd = "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/rocm6.0"
+      cmd = "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/rocm6.2"
     } 
   }
   return {
