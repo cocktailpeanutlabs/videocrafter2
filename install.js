b60251fe912ef516d640d53ba13d31285211b82c
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-19 19:59:30 -0700

    update

--- install.js
+++ install.js
@@ -1,10 +1,23 @@
 module.exports = async (kernel) => {
+  let cmd = "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu"
+  if (kernel.platform === 'darwin') {
+    if (kernel.arch === "arm64") {
+      cmd = "uv pip install torch torchaudio torchvision"
+    } else {
+      cmd = "uv pip install torch==2.1.2 torchaudio==2.1.2"
+    }
+  } else {
+    if (kernel.gpu === 'nvidia') {
+      if (kernel.gpu_model && / 50.+/.test(kernel.gpu_model)) {
+        cmd = "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
+      } else {
+        cmd = "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 xformers --index-url https://download.pytorch.org/whl/cu121"
+      }
+    } else if (kernel.gpu === 'amd') {
+      cmd = "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/rocm6.0"
+    } 
+  }
   return {
-    "cmds": {
-      "nvidia": "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 xformers --index-url https://download.pytorch.org/whl/cu124",
-      "amd": "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/rocm6.2",
-      "default": "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cpu"
-    },
     "run": [{
       "method": "shell.run",
       "params": {
@@ -17,7 +30,7 @@ module.exports = async (kernel) => {
         "venv": "env",
         "message": [
           "python -m pip install pip==24.0",
-          "{{(gpu === 'nvidia' ? self.cmds.nvidia : ((gpu === 'amd' && platform === 'linux') ? self.cmds.amd : self.cmds.default))}}",
+          cmd,
           "uv pip install -r requirements.txt",
           "uv pip install {{platform === 'darwin' ? 'eva-decord' : 'decord'}}",
         ]
